5ae390315165fade83ede76037c32e793621450d
i198sasa <138637599+i198sasa@users.noreply.github.com>
2023-07-11 09:33:59 +0900

    Update 1.c

--- 1.c
+++ 1.c
@@ -7,7 +7,7 @@ int main(void)
   printf("n = ");
   scanf("%d", &n);
 
-  if( (n & 1) == 0 ){
+  if( (n & 1) == 0 ){ /* 括弧をつけないと先に計算してくれないため、n&1に括弧を付けた */
     printf("%d は偶数です\n", n);
   }
   return 0;
