1aa7763de43c6795b94fac6c2fca17c867ee9cde
i198sasa <138637599+i198sasa@users.noreply.github.com>
2023-07-12 10:47:35 +0900

    Update a.php

--- a.php
+++ a.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php
 <?php 
-  foreach(file('/usr/share/dict/linux.words') as $line){
-    // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
+  foreach(file('/usr/share/dict/words') as $line){
+    if(strpos($line, "abc") >= 0) echo $line;// strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
   } 
 ?>
